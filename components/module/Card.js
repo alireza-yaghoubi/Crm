1dcccee2caa68217b873f0cbd49052b756427c72
AliReza Yaghobi <ali.reza.yaghoubi8181@gmail.com>
2026-02-03 18:12:20 +0330

    show customers in Home Page

---
+++ components/module/Card.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+function Card({ customer }) {
+    const deleteHandeler =()=>{}
+  return (
+    <div className="card">
+      <div className="card__details">
+        <p>
+          {customer.name} {customer.lastName}
+        </p>
+        <p>{customer.email}</p>
+      </div>
+      <div className="card__buttons">
+        <button onClick={deleteHandeler}>Delete</button>
+        <Link href={`/edit/${customer._id}`}>Edit</Link>
+        <Link href={`/customer/${customer._id}`}>Details</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,5 +1,13 @@
-function HomePage({customers}) {
-  return <div>HomePage</div>;
+import Card from "../module/Card";
+
+function HomePage({ customers }) {
+  return (
+    <div>
+      {customers.map((customer) => (
+        <Card key={customer._id} customer={customer} />
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
